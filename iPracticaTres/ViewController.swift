e243f7aa5c113fb361b1d2cf700a21f48d010705
ALEXIS-PC <alexis-pc@alexis-pcs-mac.local>
2018-10-29 21:24:00 -0700

    Initial Commit

---
+++ iPracticaTres/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPracticaTres
+//
+//  Created by ALEXIS-PC on 10/29/18.
+//  Copyright © 2018 upc.edu.pe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
